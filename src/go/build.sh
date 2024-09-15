87b0eadaa8f319dd55c9486d4147bb28ebfefed7
Christian <christian@infobaleen.com>
2024-09-15 21:17:58 +0200

    Update build.sh

--- src/go/build.sh
+++ src/go/build.sh
@@ -6,6 +6,7 @@ echo $CURRENT_DIR
 
 go env GOARCH GOOS
 
+
 if [ "$DUCKDB_PLATFORM" == "osx_amd64" ]; then
 export GOOS=darwin
 export GOARCH=amd64
@@ -21,6 +22,8 @@ export GOARCH=arm64
 elif [ "$DUCKDB_PLATFORM" == "linux_amd64" ]; then
 export GOOS=linux
 export GOARCH=amd64
+else
+  exit 1
 fi
 
 go env GOARCH GOOS
@@ -29,6 +32,6 @@ go env GOARCH GOOS
 SRC_DIR="$1"
 DUCKDB_LIB_PATH=$(realpath "$2")
 ls -l $DUCKDB_LIB_PATH
-(cd "$SRC_DIR" && go mod tidy && CGO_CFLAGS="-I${DUCKDB_LIB_PATH}/" CGO_LDFLAGS="-L${DUCKDB_LIB_PATH}" CGO_ENABLED=1 GOWORK=off go build -buildmode=c-archive -o "$CURRENT_DIR" .)
+(cd "$SRC_DIR" && GOOS=$GOOS GOARCH=$GOARCH go mod tidy && GOOS=$GOOS GOARCH=$GOARCH CGO_CFLAGS="-I${DUCKDB_LIB_PATH}/" CGO_LDFLAGS="-L${DUCKDB_LIB_PATH}" CGO_ENABLED=1 GOWORK=off go build -buildmode=c-archive -o "$CURRENT_DIR" .)
 echo "Whats in current dir?"
 ls -sl "$CURRENT_DIR"
