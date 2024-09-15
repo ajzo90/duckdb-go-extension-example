483cb46f7ca0eecd43b02185c9735bcf8379dc06
Christian <christian@infobaleen.com>
2024-09-15 22:53:01 +0200

    clean up

--- src/go/build.sh
+++ src/go/build.sh
@@ -21,7 +21,4 @@ fi
 CURRENT_DIR=$(pwd)
 SRC_DIR="$1"
 DUCKDB_LIB_PATH=$(realpath "$2")
-ls -l $DUCKDB_LIB_PATH
-(cd "$SRC_DIR" && GOOS=$GOOS GOARCH=$GOARCH go mod tidy && echo "start build" && CC=$CC GOOS=$GOOS GOARCH=$GOARCH CGO_CFLAGS="-I${DUCKDB_LIB_PATH}/" CGO_LDFLAGS="-L${DUCKDB_LIB_PATH}" CGO_ENABLED=1 GOWORK=off go build -buildmode=c-archive -o "$CURRENT_DIR" .)
-echo "Whats in current dir?"
-ls -sl "$CURRENT_DIR"
+(cd "$SRC_DIR" && go mod tidy && CC=$CC GOOS=$GOOS GOARCH=$GOARCH CGO_CFLAGS="-I${DUCKDB_LIB_PATH}/" CGO_LDFLAGS="-L${DUCKDB_LIB_PATH}" CGO_ENABLED=1 GOWORK=off go build -buildmode=c-archive -o "$CURRENT_DIR" .)
