781dfbe347530af0e048ce3d8b943bea7ab3c3d6
Christian <christian@infobaleen.com>
2024-09-15 18:06:02 +0200

    debug

--- src/go/build.sh
+++ src/go/build.sh
@@ -3,3 +3,5 @@ CURRENT_DIR=$(PWD)
 SRC_DIR="$1"
 DUCKDB_LIB_PATH="$2"
 (cd "$SRC_DIR" && go mod tidy && CGO_CFLAGS="-I${DUCKDB_LIB_PATH}/" CGO_LDFLAGS="-L${DUCKDB_LIB_PATH}" CGO_ENABLED=1 GOWORK=off go build -buildmode=c-archive -o "$CURRENT_DIR" .)
+echo "Whats in current dir?"
+ls -sl "$CURRENT_DIR"
